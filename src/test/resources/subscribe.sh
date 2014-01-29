b232c6362644ee7290a6f83bd8a4c8fb3ebb0081
Christian Mader <mail@christianmader.net>
2014-01-29 15:48:20 +0100

    switched to native rdf store for changesets

--- src/test/resources/subscribe.sh
+++ src/test/resources/subscribe.sh
@@ -4,7 +4,7 @@ for var in "$@"
 do
     for entry in "$var"/*.ttl
     do
-        curl -X POST -d @"$entry" --header "Content-Type: text/turtle" http://localhost:2221/register
+        curl -X POST -d @"$entry" --header "Content-Type: text/turtle" http://test-pp-linux.semantic-web.at:2221/register
     done
 done
 
